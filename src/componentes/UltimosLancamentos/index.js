97c41ccd42c9afde77e2a7e5e670e47ec11d20d1
Gabriel Aredes <gaamoraes@sga.pucminas.br>
2025-11-21 08:36:59 -0300

    projeto final do curso

---
+++ src/componentes/UltimosLancamentos/index.js
@@ -0,0 +1,44 @@
+import { livros } from "./dadosUltimosLancamentos.js";
+import styled from "styled-components";
+import { Titulo } from "../Titulo/index.js";
+import CardRecomenda from "../CardRecomenda/Index.js";
+import imagemLivro from '../../imagens/livro2.png'
+
+const UltimosLancamentosContainer = styled.section`
+    background-color: #EBECEE;
+    padding-bottom: 20px;
+    display: flex;
+    flex-direction: column;
+`
+
+const NovosLivrosContainer = styled.div`
+    margin-top: 30px;
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    cursor: pointer;
+`
+
+function UltimosLancamentos() {
+  return (
+    <UltimosLancamentosContainer>
+      <Titulo 
+        cor={'#EB9B00'} 
+        tamanhoFonte={'38px'}
+      >ÚLTIMOS LANÇAMENTOS</Titulo>
+      <NovosLivrosContainer>
+        {livros.map((livro) => (
+        <img src={livro.src} alt="livro" />
+      ))}
+      </NovosLivrosContainer>
+      <CardRecomenda
+        titulo="Talvez você se interesse por"
+        subtitulo="Angular 11"
+        descricao="Construindo uma aplicação com a plataforma Google"
+        img={imagemLivro}
+      />
+    </UltimosLancamentosContainer>
+  );
+}
+
+export default UltimosLancamentos;
