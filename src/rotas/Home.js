4b3c220ea6b1309dc127c1e57c3fac68561bb976
Gabriel Aredes <gaamoraes@sga.pucminas.br>
2025-11-26 09:04:47 -0300

    Add routing and new components for Home and Favoritos pages
    - Implemented react-router-dom for navigation
    - Created Home and Favoritos components
    - Updated Header and OpcoesHeader to include links
    - Added axios and react-router-dom dependencies
    - Removed old App component

--- src/rotas/Home.js
+++ src/rotas/Home.js
@@ -1,7 +1,6 @@
-import Header from './componentes/Header'
 import styled from 'styled-components'
-import Pesquisa from './componentes/Pesquisa';
-import UltimosLancamentos from './componentes/UltimosLancamentos'
+import Pesquisa from '../componentes/Pesquisa';
+import UltimosLancamentos from '../componentes/UltimosLancamentos'
 
 const AppContainer = styled.div`
   width: 100vw;
@@ -13,14 +12,13 @@ li {
 }
 `
 
-function App() {
+function Home() {
   return (
     <AppContainer>
-      <Header/>
       <Pesquisa/>
       <UltimosLancamentos />
     </AppContainer>
   );
 }
 
-export default App;
+export default Home;
